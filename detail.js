f10717f5fa4c209cd5f58f27d9bd86850dcb6ee0
Eliott <fbgus333@naver.com>
2022-03-31 23:27:47 +0900

    Today I learnd

--- detail.js
+++ detail.js
@@ -12,23 +12,24 @@ for (let i = 0; i < 3; i++){
 
 }
 
-// 버튼.eq(0).on('click', function(){
-//   버튼.removeClass('orange');
-//   버튼.eq(0).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(0).addClass('show');
-// });
+let car = ['소나타', 50000, 'white'];
+let car2 = {name : '소나타', price: 50000}
 
-// 버튼.eq(1).on('click', function(){
-//   버튼.removeClass('orange');
-//   버튼.eq(1).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(1).addClass('show');
-// });
 
-// 버튼.eq(2).on('click', function(){
-//   버튼.removeClass('orange');
-//   버튼.eq(2).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(2).addClass('show');
-// });
+
+
+let pants = [28, 30, 32];
+let a = document.querySelectorAll('.form-select');
+let str = '';
+
+a[0].addEventListener('input', function() {
+  if (a[0].value == '셔츠') {
+    a[1].classList.remove('form-hide')
+  } else if (a[0].value == '바지') {
+    a[1].classList.remove('form-hide');
+    a[1].innerHTML = '';
+    for (let i = 0; i < pants.length; i++) {
+      
+    } 
+  } 
+});
