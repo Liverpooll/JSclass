1217b80f7ffab38bf78cf91ffd7ed8def4209e67
Eliott <fbgus333@naver.com>
2022-04-04 21:31:15 +0900

    Today I learnd

--- detail/detail.js
+++ detail/detail.js
@@ -19,17 +19,42 @@ let car2 = {name : '소나타', price: 50000}
 
 
 let pants = [28, 30, 32];
+let shirts = [95, 100];
 let a = document.querySelectorAll('.form-select');
-let str = '';
+
+
+// a[0].addEventListener('input', function() {
+//   if (a[0].value == '셔츠') {
+//     a[1].classList.remove('form-hide')
+//   } else if (a[0].value == '바지') {
+//     a[1].classList.remove('form-hide');
+//     pants.forEach(function(b, i){
+//      a[1].innerHTML = "<option>" + b + "</option>"
+//      a[1].innerHTML = "<option>" + b + "</option>"
+//      a[1].innerHTML = "<option>" + b + "</option>"
+//   })
+//   }
+// });
+
 
 a[0].addEventListener('input', function() {
-  if (a[0].value == '셔츠') {
-    a[1].classList.remove('form-hide')
-  } else if (a[0].value == '바지') {
+   if (a[0].value == '바지') {
     a[1].classList.remove('form-hide');
-    a[1].innerHTML = '';
-    pants.forEach(function(b){
-     $('.form-select').eq(1).append(`<option>${b}</option>`)
-  })
-} 
-});
+    let str = '';
+    for (let i = 0; i < pants.length; i++) {
+      str = str + "<option>" + pants[i] + "</option>"}
+      a[1].classList.remove('form-hide');}
+    else if (a[0].value == '셔츠') {
+     a[1].innerHTML = str;
+    str = '';
+    for (let i = 0; i < shirts.length; i++) {
+      str = str + "<option>" + shirts[i] + "</option>"
+    } a[1].innerHTML = str;}
+    else if (a[0].value == '모자') {
+      a[1].classList.add('form-hide');
+    }
+  }
+
+);
+
+
